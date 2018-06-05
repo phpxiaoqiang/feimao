d3bd060ca6a13c3f2ca61c46b726c1430141e5a4
wxq <phpxiaoqiang@sina.com>
2018-06-05 17:39:58 +0800

    wxq

---
+++ 5Gyanjiang/js/common.js
@@ -0,0 +1,77 @@
+$(function(){
+	var curPage=$(".arrow_up").attr('curPage');
+	$("body").on("touchstart", function(e) {
+	 e.preventDefault();
+	 startX = e.originalEvent.changedTouches[0].pageX,
+	 startY = e.originalEvent.changedTouches[0].pageY;
+	});
+	$("body").on("touchmove", function(e) {
+	 e.preventDefault();
+	 moveEndX = e.originalEvent.changedTouches[0].pageX,
+	 moveEndY = e.originalEvent.changedTouches[0].pageY,
+	 X = moveEndX - startX,
+	 Y = moveEndY - startY;
+	 if(Math.abs(X)<Math.abs(Y)){
+	 	if ( Y < 0 ) { //向上滑
+	 	  if(curPage=='2'){
+	 	  	location.href="three.html";
+	 	  }else if(curPage=='3'){
+	 	  	location.href="four.html";
+	 	  }else if(curPage=='4'){
+	 	  	location.href="five.html";
+	 	  }else if(curPage=='5'){
+	 	  	location.href="six.html";
+	 	  }else if(curPage=='6'){
+	 	  	location.href="seven.html";
+	 	  }
+		}else if(Y > 0){ //向下滑
+		  if(curPage=='2'){
+	 	  	location.href="one.html";
+	 	  }else if(curPage=='3'){
+	 	  	location.href="two.html";
+	 	  }else if(curPage=='4'){
+	 	  	location.href="three.html";
+	 	  }else if(curPage=='5'){
+	 	  	location.href="four.html";
+	 	  }else if(curPage=='6'){
+	 	  	location.href="five.html";
+	 	  }else if(curPage=='7'){
+	 	  	location.href="six.html";
+	 	  }
+		}
+	 }
+	});
+});
+//var startX;
+//var startY;
+//function handleTouchEvent(event) {
+//  switch (event.type){
+//      case "touchstart":
+//          startX = event.touches[0].pageX;
+//          startY = event.touches[0].pageY;
+//          break;
+//      case "touchend":
+//          var spanX = event.changedTouches[0].pageX - startX;
+//          var spanY = event.changedTouches[0].pageY - startY;
+//
+//          if(Math.abs(spanX) > Math.abs(spanY)){      //认定为水平方向滑动
+//              if(spanX > 30){         //向右
+//                  myRight();
+//              } else if(spanX < -30){ //向左
+//                  myLeft();
+//              }
+//          } else {        //认定为垂直方向滑动
+//              if(spanY > 30){         //向下
+//                  myDown();
+//              } else if (spanY < -30) {//向上
+//                  myUp();
+//              }
+//          }
+//
+//          break;
+//      case "touchmove":
+//          //阻止默认行为
+//          event.preventDefault();
+//          break;
+//  }
+//}
---
+++ 5Gyanjiang/js/index.js
@@ -0,0 +1,16 @@
+var path = $('#path');
+
+Draggable.create("#indicator", {
+  type: "rotation",
+  throwProps: true,
+  maxRotation: 360,
+  onDrag: function() {
+    var i = this.rotation;
+    var angle = 620*(1-i/360);
+    path.css('stroke-dashoffset', angle);
+//  console.log(620*(1-i/360));
+    if(angle<330&&angle>323||angle>943&&angle<951){
+    	location.href="two.html";
+    }
+  },
+});
